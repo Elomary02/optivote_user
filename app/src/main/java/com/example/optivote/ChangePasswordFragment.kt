dfd69082f012b7aa1afb31670cb8c8699381a480
tariqazdoud <2003010101737@ofppt-edu.ma>
2024-04-16 16:12:56 +0100

    change password page done

--- app/src/main/java/com/example/optivote/ChangePasswordFragment.kt
+++ app/src/main/java/com/example/optivote/ChangePasswordFragment.kt
@@ -6,13 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class HomePageFragment : Fragment() {
+
+class ChangePasswordFragment : Fragment() {
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_change_password, container, false)
+
     }
+
 
 }
