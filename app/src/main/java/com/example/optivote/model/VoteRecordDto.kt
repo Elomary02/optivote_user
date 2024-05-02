9ee081ed58fe313dd893af248793b9b3f26af3ce
ELHOUCINE ELOMARY <elhoucineelomary@gmail.com>
2024-05-02 13:06:32 +0100

    created project classes at model package

--- app/src/main/java/com/example/optivote/model/VoteRecordDto.kt
+++ app/src/main/java/com/example/optivote/model/VoteRecordDto.kt
@@ -3,6 +3,6 @@ package com.example.optivote.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-class VoteRecordDto (val recordId: Int,
+data class VoteRecordDto (val recordId: Int,
                      val vote: VoteDto,
                      val recordDectionnary: Map<UserDto, String>)
