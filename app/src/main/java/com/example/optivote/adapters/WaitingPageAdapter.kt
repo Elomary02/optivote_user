44ad9e8deac2d1acfc4d011108d7cfd65667372d
tariqazdoud <2003010101737@ofppt-edu.ma>
2024-05-18 18:39:35 +0100

    waiting page files deleted

--- app/src/main/java/com/example/optivote/adapters/WaitingPageAdapter.kt
+++
@@ -1,4 +0,0 @@
-package com.example.optivote.adapters
-
-class WaitingPageAdapter {
-}
--- app/src/main/java/com/example/optivote/repository/WaitingPageRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.optivote.repository
-
-import com.example.optivote.model.VoteDto
-import kotlinx.coroutines.flow.Flow
-
-interface WaitingPageRepository {
-    suspend fun getVoteByCode(code:Int):Flow<VoteDto>
-}
